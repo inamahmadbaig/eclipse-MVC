72b296007f1dce06d684af05a1444e4980c4f3ea
inamahmadbaig <inamahmadbaig@gmail.com>
2026-07-13 14:23:28 +0530

    hhh

--- MVCPro01-Webfirst/src/main/java/com/nit/controller/HelloFirst.java
+++ MVCPro01-Webfirst/src/main/java/com/nit/controller/HelloFirst.java
@@ -3,13 +3,15 @@ package com.nit.controller;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloFirst {
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String firsitPage() {
 		LocalDateTime s;
+		
 		return"hi";
 	}
 
